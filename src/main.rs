7e3534b8abc9d8ed841e34a40eeea9f83409042f
beyarkay <boydrkane@gmail.com>
2023-07-09 21:14:30 +0200

    Add jq

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,12 @@
+use jq_rs;
+
 #[macro_use]
 extern crate rocket;
+
+#[get("/jq/<query>/<json>")]
+fn jq(query: String, json: String) -> Result<String, String> {
+    jq_rs::run(&query, &json).map_err(|e| e.to_string())
+}
 
 #[get("/")]
 fn index() -> &'static str {
@@ -8,7 +15,7 @@ fn index() -> &'static str {
 
 #[shuttle_runtime::main]
 async fn rocket() -> shuttle_rocket::ShuttleRocket {
-    let rocket = rocket::build().mount("/", routes![index]);
+    let rocket = rocket::build().mount("/", routes![index, jq]);
 
     Ok(rocket.into())
 }
