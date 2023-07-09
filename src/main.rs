4747bcc8b287ec6ff5079a155811964b7c3e8284
beyarkay <boydrkane@gmail.com>
2023-07-09 21:39:23 +0200

    Add URL parsing

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,34 @@
 use jq_rs;
+use shuttle_runtime::tracing::info;
 
 #[macro_use]
 extern crate rocket;
 
+#[get("/api/<url>?<q>")]
+fn jqapi(url: String, q: String) -> Result<String, String> {
+    info!("Got request /api/{url}?q={q}");
+    // "https://httpbin.org/ip"
+    let json = reqwest::blocking::get(url)
+        .map_err(|e| e.to_string())?
+        .text()
+        .map_err(|e| e.to_string())?;
+    do_jq(json, q)
+}
+
 #[get("/<json>?<q>")]
-fn jq(q: String, json: String) -> Result<String, String> {
+fn jq(json: String, q: String) -> Result<String, String> {
+    info!("Got request /{json}?q={q}");
+    do_jq(json, q)
+}
+
+fn do_jq(json: String, q: String) -> Result<String, String> {
+    info!("Running jq on {json} with {q}");
     jq_rs::run(&q, &json).map_err(|e| e.to_string())
 }
 
 #[shuttle_runtime::main]
 async fn rocket() -> shuttle_rocket::ShuttleRocket {
-    let rocket = rocket::build().mount("/", routes![jq]);
+    let rocket = rocket::build().mount("/", routes![jq, jqapi]);
 
     Ok(rocket.into())
 }
