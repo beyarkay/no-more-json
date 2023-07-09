1702c55230f8832f75e7f622c93a64aa480f7d5a
beyarkay <boydrkane@gmail.com>
2023-07-09 21:24:06 +0200

    Move query to the query param ...?<query>

--- src/main.rs
+++ src/main.rs
@@ -3,19 +3,14 @@ use jq_rs;
 #[macro_use]
 extern crate rocket;
 
-#[get("/jq/<query>/<json>")]
+#[get("/<json>?<query>")]
 fn jq(query: String, json: String) -> Result<String, String> {
     jq_rs::run(&query, &json).map_err(|e| e.to_string())
 }
 
-#[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
-}
-
 #[shuttle_runtime::main]
 async fn rocket() -> shuttle_rocket::ShuttleRocket {
-    let rocket = rocket::build().mount("/", routes![index, jq]);
+    let rocket = rocket::build().mount("/", routes![jq]);
 
     Ok(rocket.into())
 }
